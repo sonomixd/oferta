4e6360f12884678b99e9cd7d448f273c4798c473
sokolikonomi <sokol_ikonomi@yahoo.com>
2020-07-12 13:36:29 +0200

    initi flutter project

---
+++ android/app/src/main/kotlin/com/example/oferta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oferta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/configuration.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color primaryGreen = Color(0xff416d6d);
