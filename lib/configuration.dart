de020a424fca8b075cfb1c1d9d9e0475593b06d3
sokolikonomi <sokol_ikonomi@yahoo.com>
2020-07-14 15:20:27 +0200

    connected to firebase

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
 Color primaryGreen = Color(0xff416d6d);
+
+List<Map> categories = [
+  {'name': 'Cats', 'iconPath': 'images/cat.png'},
+  {'name': 'Dogs', 'iconPath': 'images/dog.png'},
+  {'name': 'Bunnies', 'iconPath': 'images/rabbit.png'},
+  {'name': 'Parrots', 'iconPath': 'images/parrot.png'},
+  {'name': 'Horses', 'iconPath': 'images/horse.png'}
+];
+
+List<BoxShadow> shadowList = [
+  BoxShadow(color: Colors.grey[300], blurRadius: 30, offset: Offset(0, 10))
+];
