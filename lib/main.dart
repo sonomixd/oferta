3dac943d60bfa9a4ce56d9978a58155717f76457
sokolikonomi <sokol_ikonomi@yahoo.com>
2020-07-17 14:50:07 +0200

    pageview and firestore working

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
-import 'home.dart';
-import 'screens/homepage.dart';
+import 'widgets/slideshow.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Oferta',
-      theme: ThemeData(
-        primaryColor: Colors.teal,
-        accentColor: Colors.white
-      ),
+      theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.white),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        body: Slideshow())
     );
   }
 }
