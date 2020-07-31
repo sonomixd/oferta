32f409ffb8ca996c0d27f32d922eea0002a77000
sokolikonomi <sokol_ikonomi@yahoo.com>
2020-07-31 16:48:35 +0200

    set orientation to only vertically

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'screens/home_slideshow_screen.dart';
+import 'package:flutter/services.dart';
+
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); 
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_){
+    runApp(MyApp());
+   });
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Oferta',
-      theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.white),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Slideshow())
-    );
+        title: 'Oferta',
+        theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.white),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(body: Slideshow()));
   }
 }
