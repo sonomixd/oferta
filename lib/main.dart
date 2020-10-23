d2e64bc63012ec7ce6364e6387b44301564f586b
sokolikonomi <sokol_ikonomi@yahoo.com>
2020-10-23 19:24:22 +0200

    refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,14 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_admob/firebase_admob.dart';
-import 'screens/home_slideshow_screen.dart';
+import 'package:oferta/screens/country_selection.dart';
 import 'package:flutter/services.dart';
 
-const String IOS_APP_ID = "ca-app-pub-1136808748888024~2566324176";
-const String ANDROID_APP_ID = "ca-app-pub-1136808748888024~6501699753";
-const String IOS_AD_UNIT_BANNER = "ca-app-pub-1136808748888024/4559074456";
-const String ANDROID_AD_UNIT_BANNER = "ca-app-pub-1136808748888024/3875536416";
-const String IOS_AD_UNIT_INTERSTITIAL =
-    "ca-app-pub-1136808748888024/5583547369";
-const String ANDROID_AD_UNIT_INTERSTITIAL =
-    "ca-app-pub-1136808748888024/2503756758";
-
-String getAppId() {
-  if (Platform.isIOS) {
-    return IOS_APP_ID;
-  } else if (Platform.isAndroid) {
-    return ANDROID_APP_ID;
-  }
-  return null;
-}
-
-String getBannerAdUnitId() {
-  if (Platform.isIOS) {
-    return IOS_AD_UNIT_BANNER;
-  } else if (Platform.isAndroid) {
-    return ANDROID_AD_UNIT_BANNER;
-  }
-  return null;
-}
-
-String getInterstitialAdUnitId() {
-  if (Platform.isIOS) {
-    return IOS_AD_UNIT_INTERSTITIAL;
-  } else if (Platform.isAndroid) {
-    return ANDROID_AD_UNIT_INTERSTITIAL;
-  }
-  return null;
-}
-
-BannerAd _bannerAd;
-InterstitialAd _interstitialAd;
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(MyApp());
@@ -58,40 +21,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
-    keywords: <String>['fashion', 'clothes'],
-  );
-
-  BannerAd createBannerAd() {
-    return BannerAd(
-      adUnitId: getBannerAdUnitId(),
-      size: AdSize.smartBanner,
-      targetingInfo: targetingInfo,
-      listener: (MobileAdEvent event) {
-        print("BannerAd event $event");
-      },
-    );
-  }
-
-  InterstitialAd createInterstitialAd() {
-    return InterstitialAd(
-      adUnitId: getInterstitialAdUnitId(),
-      targetingInfo: targetingInfo,
-      listener: (MobileAdEvent event) {
-        print("InterstitialAd event $event");
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    FirebaseAdMob.instance.initialize(appId: getAppId());
-    _bannerAd = createBannerAd()
-      ..load()
-      ..show(anchorType: AnchorType.bottom);
-    _interstitialAd = createInterstitialAd()..load();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,6 +28,6 @@ class _MyAppState extends State<MyApp> {
         title: 'Oferta',
         theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.white),
         debugShowCheckedModeBanner: false,
-        home: Scaffold(body: Slideshow()));
+        home: Scaffold(body: SelectCountry()));
   }
 }
--- lib/screens/story_details_screen.dart
+++ lib/screens/story_details_screen.dart
@@ -6,15 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
 
-const String IOS_APP_ID = "ca-app-pub-1136808748888024~2566324176";
-const String ANDROID_APP_ID = "ca-app-pub-1136808748888024~6501699753";
-const String IOS_AD_UNIT_BANNER = "ca-app-pub-1136808748888024/4559074456";
-const String ANDROID_AD_UNIT_BANNER = "ca-app-pub-1136808748888024/3875536416";
-const String IOS_AD_UNIT_INTERSTITIAL =
-    "ca-app-pub-1136808748888024/5583547369";
-const String ANDROID_AD_UNIT_INTERSTITIAL =
-    "ca-app-pub-1136808748888024/2503756758";
-
 class StoryDetailsScreen extends StatefulWidget {
   final Map data;
 
@@ -25,40 +16,7 @@ class StoryDetailsScreen extends StatefulWidget {
 }
 
 class _StoryDetailsScreenState extends State<StoryDetailsScreen> {
-  static const MobileAdTargetingInfo targetingInfo =
-      MobileAdTargetingInfo(keywords: <String>['fashion', 'clothes']);
-  final List<String> images = [
-    'assets/images/google_sign_in.png',
-    'assets/images/google_sign_in.png',
-    'assets/images/google_sign_in.png'
-  ];
-
-  int currentIndex = 0;
-
-  void _next() {
-    setState(() {
-      if (currentIndex < 2) {
-        currentIndex++;
-      } else {
-        currentIndex = currentIndex;
-      }
-    });
-  }
-
-  void _previous() {
-    setState(() {
-      if (currentIndex > 0) {
-        currentIndex--;
-      } else {
-        currentIndex = 0;
-      }
-    });
-  }
-
   String url = "";
-  MobileAdEvent _mobileAdEvent;
-  InterstitialAd _interstitialAd;
-  bool showInterstitialAd = false;
 
   @override
   Widget build(BuildContext context) {
@@ -69,138 +27,81 @@ class _StoryDetailsScreenState extends State<StoryDetailsScreen> {
             child: BlurHash(
               imageFit: BoxFit.cover,
               hash: "TICSbN9Maf~QNIj?-lkAkAxYjbod",
-              image: widget.data['images'][currentIndex],
+              image: widget.data['image_url'],
             ),
-            //   child: FadeInImage.assetNetwork(
-            //       height: 450,
-            //       fit: BoxFit.cover,
-            //       placeholder: 'assets/images/loading-leaf.png',
-            //       image: widget.data['images'][currentIndex]
-            //       ),
           ),
-          GestureDetector(
-            onHorizontalDragEnd: (DragEndDetails details) {
-              if (details.velocity.pixelsPerSecond.dx > 0) {
-                _previous();
-              } else if (details.velocity.pixelsPerSecond.dx < 0) {
-                _next();
-              }
-            },
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.7,
             child: Container(
               width: double.infinity,
-              height: 600,
-              margin: EdgeInsets.only(bottom: 10),
-              // decoration: BoxDecoration(
-              //     borderRadius: BorderRadius.circular(10),
-              //     image: DecorationImage(
-              //       image: NetworkImage(widget.data['images'][currentIndex]),
-              //       fit: BoxFit.cover,
-              //     ),
-              //     ),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-                    Colors.white.withOpacity(.0),
-                    Colors.white.withOpacity(.2),
-                  ]),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: 40,
-                      margin: EdgeInsets.only(top: 35),
-                      child: Row(
-                        children: _buildIndicator(),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    widget.data['title'],
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.grey[800],
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        widget.data['price'],
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            decoration: TextDecoration.lineThrough),
                       ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Align(
-            child: Transform.translate(
-              offset: Offset(0, 330),
-              child: Container(
-                height: 500,
-                width: double.infinity,
-                padding: EdgeInsets.all(30),
-                alignment: Alignment.topLeft,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      widget.data['title'],
-                      style: TextStyle(
-                          fontSize: 30,
-                          color: Colors.grey[800],
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          widget.data['product_price'],
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              decoration: TextDecoration.lineThrough),
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              widget.data['product_sale_price'],
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      child: Align(
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.green,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: ButtonTheme(
-                              minWidth: 300.0,
-                              child: FlatButton(
-                                  color: Colors.green,
-                                  child: Text(
-                                    "GO TO STORE",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  onPressed: () {
-                                    Text text = Text(widget.data['url']);
-                                    _launchUniversalLinks(text.data);
-                                  }),
-                            ),
-                          ),
-                        ),
+                      SizedBox(
+                        width: 50,
                       ),
-                    )
-                  ],
-                ),
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            widget.data['sale_price'],
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 30)),
+                  Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    padding: EdgeInsets.all(6),
+                    child: ButtonTheme(
+                      minWidth: MediaQuery.of(context).size.width * 0.8,
+                      height: 30,
+                      child: FlatButton(
+                          color: Colors.green,
+                          child: Text(
+                            "GO TO STORE",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            Text text = Text(widget.data['url']);
+                            _launchUniversalLinks(text.data);
+                          }),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -220,14 +121,6 @@ class _StoryDetailsScreenState extends State<StoryDetailsScreen> {
     );
   }
 
-  Future<void> _launchInBrowser(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url, forceSafariVC: false, forceWebView: false);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   Future<void> _launchUniversalLinks(String url) async {
     if (await canLaunch(url)) {
       final bool appLaunchedSuccessfully =
@@ -237,66 +130,5 @@ class _StoryDetailsScreenState extends State<StoryDetailsScreen> {
         await launch(url, forceSafariVC: true);
       }
     }
-  }
-
-  Widget _indicator(bool isActive) {
-    return Expanded(
-      child: Container(
-        height: 10,
-        margin: EdgeInsets.only(left: 4),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: isActive ? Colors.green : Colors.white),
-      ),
-    );
-  }
-
-  List<Widget> _buildIndicator() {
-    List<Widget> indicators = [];
-    for (int i = 0; i < 3; i++) {
-      if (currentIndex == i) {
-        indicators.add(_indicator(true));
-      } else {
-        indicators.add(_indicator(false));
-      }
-    }
-    return indicators;
-  }
-
-  String getInterstitialAdUnitId() {
-    if (Platform.isIOS) {
-      return IOS_AD_UNIT_INTERSTITIAL;
-    } else if (Platform.isAndroid) {
-      return ANDROID_AD_UNIT_INTERSTITIAL;
-    }
-    return null;
-  }
-
-  InterstitialAd createInterstitialAd() {
-    return InterstitialAd(
-      adUnitId: getInterstitialAdUnitId(),
-      targetingInfo: targetingInfo,
-      listener: (_mobileAdEvent) {
-        print("InterstitialAd event $_mobileAdEvent");
-      },
-    );
-  }
-
-  String getAppId() {
-    if (Platform.isIOS) {
-      return IOS_APP_ID;
-    } else if (Platform.isAndroid) {
-      return ANDROID_APP_ID;
-    }
-    return null;
-  }
-
-  @override
-  void initState() {
-    FirebaseAdMob.instance.initialize(appId: getAppId());
-    _interstitialAd = createInterstitialAd()
-      ..load()
-      ..show();
-    super.initState();
   }
 }
